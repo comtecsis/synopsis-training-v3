2832a14fb19e48a9e057994292d46f2da7ff347f
Elvis Perez <ebardalesdj@mpfn.gob.pe>
2021-09-02 17:40:26 -0500

    Preparacion de formulario de cliente

--- src/main/java/ws/synopsis/training/controller/TrainingController.java
+++ src/main/java/ws/synopsis/training/controller/TrainingController.java
@@ -1,6 +1,7 @@
 package ws.synopsis.training.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import ws.synopsis.training.model.Client;
@@ -14,9 +15,10 @@ public class TrainingController {
     }
 
     @PostMapping
-    public String index(Client client){
-
-        return "client";
+    public String index(Client client, Model model){
+        model.addAttribute("name", client.getName());
+        model.addAttribute("email", client.getEmail());
+        return "client/index";
     }
 
 }
