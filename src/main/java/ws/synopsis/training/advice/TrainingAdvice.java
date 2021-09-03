6244a4c49a19c383eef075dd919a072d8ef7a9d1
Elvis Perez <ebardalesdj@mpfn.gob.pe>
2021-09-03 18:16:55 -0500

    Coptura de errores no controlados

--- src/main/java/ws/synopsis/training/advice/TrainingAdvice.java
+++ src/main/java/ws/synopsis/training/advice/TrainingAdvice.java
@@ -1,4 +1,15 @@
 package ws.synopsis.training.advice;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class TrainingAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> exceptionHandler(Exception e){
+        return ResponseEntity.ok().body("Captura de error general.");
+    }
+
 }
