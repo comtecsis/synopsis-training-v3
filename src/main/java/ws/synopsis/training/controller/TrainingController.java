d7d8eeed562d31cb780545574ca1f4d2c843b270
KevinFloresF <55821372+KevinFloresF@users.noreply.github.com>
2021-09-02 23:17:06 -0500

    add POST GET Thymeleaf

--- src/main/java/ws/synopsis/training/controller/TrainingController.java
+++ src/main/java/ws/synopsis/training/controller/TrainingController.java
@@ -1,20 +1,25 @@
 package ws.synopsis.training.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ws.synopsis.training.model.Client;
 
 @Controller
 public class TrainingController {
 
-    @GetMapping
-    public String index(){
+    @GetMapping({"/index","/"})
+    public String index(Model model) {
+        model.addAttribute("client", new Client());
         return "index";
     }
 
-    @PostMapping
-    public String index(Client client){
+    @PostMapping("/client")
+    public String client(@ModelAttribute Client client, Model model) {
+
+        model.addAttribute("client", client);
 
         return "client";
     }
