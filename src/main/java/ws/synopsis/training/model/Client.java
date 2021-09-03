35bfeee69d2f6cb77bc230d14b22516f67ee9f22
wsa01 <wilder.salinas.ap@gmail.com>
2021-09-02 20:38:15 -0500

    formulario con metodos GET y POST

--- src/main/java/ws/synopsis/training/model/Client.java
+++ src/main/java/ws/synopsis/training/model/Client.java
@@ -1,9 +1,34 @@
 package ws.synopsis.training.model;
 
-import lombok.Data;
+//import lombok.Data;
 
-@Data
+//@Data
 public class Client {
     private String name;
     private String email;
+    public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Client(String name, String email) {
+		super();
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
